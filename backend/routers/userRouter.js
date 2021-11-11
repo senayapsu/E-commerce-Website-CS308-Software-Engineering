fd6e02a4aaf3082f2541ef8bb59b5103f2f667dc
Sena Yapsu <80310237+senayapsu@users.noreply.github.com>
2021-11-11 18:10:54 +0300

    Backend all files
    Backend all

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -9,7 +9,7 @@ const userRouter = express.Router();
 
 //get method to see API
 userRouter.get(
-  '/Login',
+  '/seed',
   expressAsyncHandler(async (req, res) => {
     //await User.remove({});
     const createdUsers = await User.insertMany(data.users);
